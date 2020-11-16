22b32dd37fd8ee97876cb7b0676c87f2ec4dd6b4
Nilda <nick.ovalles@gmail.com>
2020-11-15 17:23:31 -0700

    <variables added. name brainstorm>

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,45 @@
 // Assignment Code Here
 
+//var passwordLength????
+//CreatePassword?
+
+// Create Variables For Password Character Choices
+function createPassword(#, #){
+password = "";
+var special = "!@#$%^&*()_-=+`~[{}]|:;'<,>.?/";
+var number = "1234567890";
+var uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lowercase = "qwertyuiopasdfghjklzxcvbnm";
+
+}
+
+
+
+
+// Generate Appropriate Password based on charType (check w3school for that link)
+
+
+
+// Prompt User To Select One of the Following Options
+
+
+// Use Swtich to Carry out Action
+
+
+// Generate Password Based on all Factors
+function generatePassword() {
+  var passwordLength = charCount();
+  if (passwordLength === null) {
+    return null;
+  }
+
+  var password = charType(passwordLength);
+  if (password === null) {
+    return null;
+  }
+  return password;
+}
+
 
 // Get References to the #Generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,7 +49,14 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //If a password was not selected... write "The User Cancelled During The Prompt"
+  if (password === null) {
+    passwordText.value = "The User Cancelled During The Prompt.";
+
+    // If not... copy the password into the text box.
+  } else {
+    passwordText.value = password;
+  }
 
 }
 
